213c6a0e7da8567a6aecabac972bbd643e7804ee
Fernando Nieva <nieva.cronos@gmail.com>
2023-04-26 16:06:53 -0300

    first commit

---
+++ src/todos/app.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+export const App = (elementId)=>{
+
+        //cuando la funcion app ()se llama
+        (()=>{
+
+            const app = document.createElement('div');
+            app.innerHTML= '<h1>Hola Mundo</h1>';
+            document.querySelector(elementId).append(app)
+
+
+
+        })();
+}
